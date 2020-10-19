9a32498a887791e52c00fd413ee232dbe78cd409
Cássio Cappellari <cassiocappellari@gmail.com>
2020-10-19 19:17:45 -0300

    happy app first commit

---
+++ backend/src/controllers/OrphanagesController.ts
@@ -0,0 +1,48 @@
+import {Request, Response} from 'express'
+import {FindOperator, getRepository} from 'typeorm'
+import Orphanage from '../models/Orphanages'
+
+export default {
+    async index(req: Request, res: Response) { // rota de listagem de orfanatos
+        const orphanagesRepository = getRepository(Orphanage)
+
+        const orphanages = await orphanagesRepository.find()
+
+        return res.json(orphanages)
+    },
+    async show(req: Request, res: Response) {
+        const {id} = req.params
+
+        const orphanagesRepository = getRepository(Orphanage)
+        const orphanage = await orphanagesRepository.findOneOrFail(id)
+
+        return res.json(orphanage)
+    },
+    async create(req: Request, res: Response) {
+        const {
+            name,
+            latitude,
+            longitude,
+            about,
+            instructions,
+            opening_hours,
+            open_on_weekends
+        } = req.body
+
+        const orphanagesRepository = getRepository(Orphanage)
+
+        const orphanage = orphanagesRepository.create({
+            name,
+            latitude,
+            longitude,
+            about,
+            instructions,
+            opening_hours,
+            open_on_weekends
+        })
+
+        await orphanagesRepository.save(orphanage)
+
+        return res.status(201).json(orphanage)
+    }
+}
